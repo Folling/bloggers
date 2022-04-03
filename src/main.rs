4e29669a58aa54c7de6881962ca90e3ec270c1f6
Folling <coding@folling.de>
2022-04-03 22:41:05 +0200

    Setup cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::*;
+use simplelog::*;
+
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+fn main() -> Result<()> {
+    TermLogger::init(
+        LevelFilter::Debug,
+        ConfigBuilder::new().clear_filter_ignore().build(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )?;
+
+    info!("Hello, world!");
+
+    Ok(())
 }
