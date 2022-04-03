85c4171dab2125891ced52b2ba9d85226b00091b
Folling <coding@folling.de>
2022-04-03 23:37:49 +0200

    Add cargo/clippy flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#![feature(register_tool)]
+#![register_tool(rust_analyzer)]
+// Enable clippy if our Cargo.toml file asked us to do so.
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+// Enable as many useful Rust and Clippy warnings as we can stand.  We'd
+// also enable `trivial_casts`, but we're waiting for
+// https://github.com/rust-lang/rust/issues/23416.
+#![warn(
+    missing_debug_implementations,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![cfg_attr(feature = "clippy", warn(cast_possible_truncation))]
+#![cfg_attr(feature = "clippy", warn(cast_possible_wrap))]
+#![cfg_attr(feature = "clippy", warn(cast_precision_loss))]
+#![cfg_attr(feature = "clippy", warn(cast_sign_loss))]
+#![cfg_attr(feature = "clippy", warn(missing_docs_in_private_items))]
+#![cfg_attr(feature = "clippy", warn(mut_mut))]
+// Disallow `println!`. Use `debug!` for debug output
+// (which is provided by the `log` crate).
+#![cfg_attr(feature = "clippy", warn(print_stdout))]
+// This allows us to use `unwrap` on `Option` values (because doing makes
+// working with Regex matches much nicer) and when compiling in test mode
+// (because using it in tests is idiomatic).
+#![cfg_attr(all(not(test), feature = "clippy"), warn(result_unwrap_used))]
+#![cfg_attr(feature = "clippy", warn(unseparated_literal_suffix))]
+#![cfg_attr(feature = "clippy", warn(wrong_pub_self_convention))]
+
 use anyhow::*;
 
 use log::Level;
